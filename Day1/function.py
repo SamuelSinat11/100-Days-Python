e23d6f4a11e56e7eda2eb463201cf40995d89112
Samuel Sinat <106652156+SamuelSinat11@users.noreply.github.com>
2024-04-09 14:29:13 +0700

    learning the def put in function

--- Day1/function.py
+++ Day1/function.py
@@ -17,3 +17,33 @@ def greet_again(name):
     return result
 
 assert greet_again("Jonh") == "Hello, Jonh"
+
+
+# Functions can return other functions. In other words, functions generating other functions 
+
+def compose_greet_func(): 
+    def get_message(): 
+        return 'Hello there!'
+    
+    return get_message 
+
+greet_function = compose_greet_func()
+assert greet_function() == 'Hello there!'
+
+
+# Inner functions have access to the enclosing scope. 
+
+# More commonly know as a closure. A very powerful pattern that we will come across while 
+# building decorators. Another thing to note, python only allows read access to the outer 
+# scope and not assigment. Notice how we modified the example above to read a "name" argument 
+# from the enclosing scope of the inner function and return the new function. 
+
+def compose_greet_func_with_closure(name): 
+    def get_message(): 
+        return "Hello" + name + "!"
+
+    return get_message
+
+greet_with_closure = compose_greet_func_with_closure("Jonh")
+assert greet_with_closure() == "Hello there, Jonh! "
+
